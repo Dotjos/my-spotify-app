11105107b7ce7f049697c618f9a362a6aa7c5518
Jose <oladotjos@gmail.com>
2025-09-25 11:10:52 +0100

    style structure-In place

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from "next";
+import { Quicksand } from "next/font/google";
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-quicksand",
+});
 
 export const metadata: Metadata = {
   title: "Music-Spotify-App",
@@ -12,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={quicksand.className}>
       <body className="">{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className=""></div>;
+  return <div className="font-bold text-6xl">R$B</div>;
 }
